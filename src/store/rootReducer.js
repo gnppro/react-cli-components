5335bfdb64fe0d395cf429785ce422bb65f3ed1d
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2020-07-08 09:51:48 -0500

    🔥 [Remove] unnecesary files

--- src/store/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { connectRouter } from 'connected-react-router';
-import history from './history';
-/* INJECT_IMPORT_REDUCER */
-
-export default combineReducers({
-  router: connectRouter(history),
-  /* INJECT_REDUCER */
-});
