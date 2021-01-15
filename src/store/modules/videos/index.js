d020c615f7ff38bc130c5879a31f4619c7f708e5
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2021-01-15 01:09:00 -0600

    🔥 [Remove] Files

--- src/store/modules/videos/index.js
+++
@@ -1,8 +0,0 @@
-import reducer from './reducer';
-import * as types from './types';
-import * as actions from './actions';
-import { sagas } from './sagas';
-
-export { types, actions, sagas };
-
-export default reducer;
